f980eec2bae2ea6f96c99ce75f5dbad1c5e21765
itsvyle <itsvyle@users.noreply.github.com>
2024-03-10 23:37:43 +0000

    [auto] Update compiled version

--- dist/index.js
+++ dist/index.js
@@ -7848,40 +7848,42 @@ const Stylelint = __nccwpck_require__(194);
 const SwiftFormatLockwood = __nccwpck_require__(8983);
 const SwiftFormatOfficial = __nccwpck_require__(1828);
 const SwiftLint = __nccwpck_require__(1439);
+const StaticCheck = __nccwpck_require__(4760);
 const TSC = __nccwpck_require__(7540);
 const XO = __nccwpck_require__(728);
 
 const linters = {
-	// Linters
-	clippy: Clippy,
-	erblint: Erblint,
-	eslint: ESLint,
-	flake8: Flake8,
-	golint: Golint,
-	mypy: Mypy,
-	php_codesniffer: PHPCodeSniffer,
-	pylint: Pylint,
-	rubocop: RuboCop,
-	stylelint: Stylelint,
-	swiftlint: SwiftLint,
-	xo: XO,
-	tsc: TSC,
+  // Linters
+  clippy: Clippy,
+  erblint: Erblint,
+  eslint: ESLint,
+  flake8: Flake8,
+  golint: Golint,
+  mypy: Mypy,
+  php_codesniffer: PHPCodeSniffer,
+  pylint: Pylint,
+  rubocop: RuboCop,
+  stylelint: Stylelint,
+  swiftlint: SwiftLint,
+  staticcheck: StaticCheck,
+  xo: XO,
+  tsc: TSC,
 
-	// Formatters (should be run after linters)
-	autopep8: Autopep8,
-	black: Black,
-	clang_format: ClangFormat,
-	dotnet_format: DotnetFormat,
-	gofmt: Gofmt,
-	oitnb: Oitnb,
-	rustfmt: RustFmt,
-	prettier: Prettier,
-	swift_format_lockwood: SwiftFormatLockwood,
-	swift_format_official: SwiftFormatOfficial,
+  // Formatters (should be run after linters)
+  autopep8: Autopep8,
+  black: Black,
+  clang_format: ClangFormat,
+  dotnet_format: DotnetFormat,
+  gofmt: Gofmt,
+  oitnb: Oitnb,
+  rustfmt: RustFmt,
+  prettier: Prettier,
+  swift_format_lockwood: SwiftFormatLockwood,
+  swift_format_official: SwiftFormatOfficial,
 
-	// Alias of `swift_format_lockwood` (for backward compatibility)
-	// TODO: Remove alias in v2
-	swiftformat: SwiftFormatLockwood,
+  // Alias of `swift_format_lockwood` (for backward compatibility)
+  // TODO: Remove alias in v2
+  swiftformat: SwiftFormatLockwood,
 };
 
 module.exports = linters;
@@ -8582,6 +8584,90 @@ class RustFmt {
 }
 
 module.exports = RustFmt;
+
+
+/***/ }),
+
+/***/ 4760:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const { run } = __nccwpck_require__(9575);
+const commandExists = __nccwpck_require__(5265);
+const { initLintResult } = __nccwpck_require__(9149);
+const { capitalizeFirstLetter } = __nccwpck_require__(9321);
+
+const PARSE_REGEX = /^(.+):([0-9]+):[0-9]+: (.+)$/gm;
+
+/** @typedef {import('../utils/lint-result').LintResult} LintResult */
+
+/**
+ * https://github.com/golang/lint
+ */
+class Staticcheck {
+	static get name() {
+		return "staticcheck";
+	}
+
+	/**
+	 * Verifies that all required programs are installed. Throws an error if programs are missing
+	 * @param {string} dir - Directory to run the linting program in
+	 * @param {string} prefix - Prefix to the lint command
+	 */
+	static async verifySetup(dir, prefix = "") {
+		// Verify that golint is installed
+		if (!(await commandExists("staticcheck"))) {
+			throw new Error(`${this.name} is not installed`);
+		}
+	}
+
+	/**
+	 * Runs the linting program and returns the command output
+	 * @param {string} dir - Directory to run the linter in
+	 * @param {string[]} extensions - File extensions which should be linted
+	 * @param {string} args - Additional arguments to pass to the linter
+	 * @param {boolean} fix - Whether the linter should attempt to fix code style issues automatically
+	 * @param {string} prefix - Prefix to the lint command
+	 * @returns {{status: number, stdout: string, stderr: string}} - Output of the lint command
+	 */
+	static lint(dir, extensions, args = "", fix = false, prefix = "") {
+		if (extensions.length !== 1 || extensions[0] !== "go") {
+			throw new Error(`${this.name} error: File extensions are not configurable`);
+		}
+
+		return run(`${prefix} staticcheck -f text ${args} "./..."`, {
+			dir,
+			ignoreErrors: true,
+		});
+	}
+
+	/**
+	 * Parses the output of the lint command. Determines the success of the lint process and the
+	 * severity of the identified code style violations
+	 * @param {string} dir - Directory in which the linter has been run
+	 * @param {{status: number, stdout: string, stderr: string}} output - Output of the lint command
+	 * @returns {LintResult} - Parsed lint result
+	 */
+	static parseOutput(dir, output) {
+		const lintResult = initLintResult();
+		lintResult.isSuccess = output.status === 0;
+
+		const matches = output.stdout.matchAll(PARSE_REGEX);
+		for (const match of matches) {
+			const [_, path, line, text] = match;
+			const lineNr = parseInt(line, 10);
+			lintResult.error.push({
+				path,
+				firstLine: lineNr,
+				lastLine: lineNr,
+				message: capitalizeFirstLetter(text),
+			});
+		}
+
+		return lintResult;
+	}
+}
+
+module.exports = Staticcheck;
 
 
 /***/ }),
